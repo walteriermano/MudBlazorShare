776132575c2d5b6ce88d649c99896e9bd76b55d6
walteriermano <42639364+walteriermano@users.noreply.github.com>
2022-02-08 05:21:35 +0100

    delete 2 , add Mudary.Confirm

--- MudServer/MudServer/Helpers/Mudary.cs
+++ MudServer/MudServer/Helpers/Mudary.cs
@@ -1,6 +1,24 @@
-﻿namespace MudServer.Helpers
+﻿using MudBlazor;
+
+namespace MudServer.Helpers
 {
     public class Mudary
     {
+        public Mudary(IDialogService DialogService)
+        {
+            this.DialogService = DialogService;
+        }
+
+        public IDialogService DialogService { get; }
+
+        public static async Task<bool?> Confirm(IDialogService DialogService)
+        {
+           
+            return  await DialogService.ShowMessageBox("Warning",
+            "Deleting can not be undone!",
+            yesText: "Delete!", cancelText: "Cancel");
+
+            
+        }
     }
 }
