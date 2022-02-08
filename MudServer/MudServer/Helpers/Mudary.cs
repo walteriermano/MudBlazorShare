3d0d081cc33aa2ddff1101b332d7642b37dcc71e
walteriermano <42639364+walteriermano@users.noreply.github.com>
2022-02-08 06:02:19 +0100

    aggiunto box DialoDelete

--- MudServer/MudServer/Helpers/Mudary.cs
+++ MudServer/MudServer/Helpers/Mudary.cs
@@ -18,9 +18,38 @@ namespace MudServer.Helpers
             yesText: yes, cancelText: cancel);            
         }
 
+        public static async Task<bool?> ConfirmWithOptions(IDialogService DialogService, string Title, string prompt, string yes, string cancel, DialogOptions options)
+        {
+
+            return await DialogService.ShowMessageBox(Title,
+            prompt,
+             yes, null, cancel, options);
+        }
+
         public static async Task<bool?> ConfirmDelete(IDialogService DialogService)
         {
-            return await Confirm(DialogService, "Attenzione!","Vuoi Eliminare questo record?","Si,Elimina!", "Annulla");
+            var options = new DialogOptions { DisableBackdropClick = false,CloseOnEscapeKey=true, CloseButton = true };
+            return await ConfirmWithOptions(DialogService, "Attenzione!","Vuoi Eliminare questo record?","Si,Elimina!", "Annulla", options);
         }
+
+        public static async Task<bool?> ConfirmDelete2(IDialogService DialogService)
+        {
+            var parameters = new DialogParameters();
+            parameters.Add("ContentText", "Do you really want to delete these records? This process cannot be undone.");
+            parameters.Add("ButtonText", "Delete");
+            parameters.Add("Color", Color.Error);
+            var options = new DialogOptions { DisableBackdropClick = false, CloseOnEscapeKey = true, CloseButton = true };
+            return await ConfirmWithParameters(DialogService,"Elimina", parameters, options);
+        }
+
+        public static async Task<bool?> ConfirmWithParameters(IDialogService DialogService, string Title,  DialogParameters parameters, DialogOptions options)
+        {
+
+            // return await DialogService.Show<DialogDelete>(Title,parameters, options);
+
+            return false;
+        }
+
+
     }
 }
